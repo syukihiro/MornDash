514d777f51979425bf1113b06c58baf9c449a337
syukihiro <kaui.yukihiro@gmail.com>
2026-01-14 22:29:27 +0900

    Initial Commit

---
+++ MornDash/MornDashApp.swift
@@ -0,0 +1,17 @@
+//
+//  MornDashApp.swift
+//  MornDash
+//
+//  Created by Yukihiro Sawada on 2026/01/14.
+//
+
+import SwiftUI
+
+@main
+struct MornDashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
